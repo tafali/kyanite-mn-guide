a01a0842af7b1d23d82a6695e6bd9b1366ac50fc
Bedri Özgür Güler <bedriguler@gmail.com>
2020-10-02 05:25:42 +0300

    Adds rpc_config.js (for kyan.conf rpc variables) and rpc_client.js (empty functions with detailed function description comments) files.

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,8 @@
 </template>
 
 <script>
-import Entry from './components/Entry';
+import RpcConfig from './components/RpcConfig';
+import Collateral from './components/Collateral';
 import BLS from './components/BLS';
 import Server from './components/Server';
 import Address from './components/Address';
@@ -44,54 +45,64 @@ export default {
   name: 'App',
 
   components: {
-    Entry,BLS,Server,Address,Prepare,Sign,Submit
+    RpcConfig,Collateral,BLS,Server,Address,Prepare,Sign,Submit
   },
 
   data: () => ({
-    c : 'Entry',
+    c : 'RpcConfig',
     d : {
-      collateralHash:'',
-      collateralIndex:'',
-      blsSecret:'',
-      blsPublic:'',
-      addrOwner:'',
-      addrVoting:'',
-      addrPayout:'',
-      addrFee:'',
-      prepTx:'',
-      prepCollateralAddress:'',
-      prepSignMessage:'',
-      signAnswer:''
+		rpcUser:'',
+		rpcPassword:'',
+
+		collateralHash:'',
+		collateralIndex:'',
+
+		blsSecret:'',
+		blsPublic:'',
+
+		addrOwner:'',
+		addrVoting:'',
+		addrPayout:'',
+		addrFee:'',
+
+		prepTx:'',
+		prepCollateralAddress:'',
+		prepSignMessage:'',
+		signAnswer:''
     }
   }),
 
   methods: {
     entryNext(v) {
-      if(this.c === 'Entry'){
-        this.d.collateralHash = v.collateralHash
-        this.d.collateralIndex = v.collateralIndex
-        this.c = 'BLS'
-      } else if(this.c === 'BLS'){
-        this.d.blsSecret = v.blsSecret
-        this.d.blsPublic = v.blsPublic
-        this.c = 'Server'
-      } else if(this.c === 'Server'){
-        this.c = 'Address'
-      } else if(this.c === 'Address'){
-        this.d.addrOwner = v.addrOwner
-        this.d.addrVoting = v.addrVoting
-        this.d.addrPayout = v.addrPayout
-        this.d.addrFee = v.addrFee
-        this.c = 'Prepare'
-      } else if(this.c === 'Prepare'){
-        this.d.prepTx = v.prepTx
-        this.d.prepCollateralAddress = v.prepCollateralAddress
-        this.d.prepSignMessage = v.prepSignMessage
-        this.c = 'Sign'
-      } else if(this.c === 'Sign'){
-        this.d.signAnswer = v.signAnswer
-        this.c = 'Submit'
-      }
+		if(this.c === 'RpcConfig'){
+			this.d.rpcUser = v.rpcUser
+			this.d.rpcPassword = v.rpcPassword
+			this.c = 'Collateral'
+		} else if(this.c === 'Collateral'){
+			this.d.collateralHash = v.collateralHash
+			this.d.collateralIndex = v.collateralIndex
+			this.c = 'BLS'
+		} else if(this.c === 'BLS'){
+			this.d.blsSecret = v.blsSecret
+			this.d.blsPublic = v.blsPublic
+			this.c = 'Server'
+		} else if(this.c === 'Server'){
+			this.c = 'Address'
+		} else if(this.c === 'Address'){
+			this.d.addrOwner = v.addrOwner
+			this.d.addrVoting = v.addrVoting
+			this.d.addrPayout = v.addrPayout
+			this.d.addrFee = v.addrFee
+			this.c = 'Prepare'
+		} else if(this.c === 'Prepare'){
+			this.d.prepTx = v.prepTx
+			this.d.prepCollateralAddress = v.prepCollateralAddress
+			this.d.prepSignMessage = v.prepSignMessage
+			this.c = 'Sign'
+		} else if(this.c === 'Sign'){
+			this.d.signAnswer = v.signAnswer
+			this.c = 'Submit'
+		}
     }
   }
 };
---
+++ src/components/RpcConfig.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container>
+
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <p>Please copy and paste your <code>rpcuser</code> and <code>rpcpassword</code> for Kyanite from kyan.conf file located in Kyanite Wallet data directory.</p>
+        <p>Leave blank if you do not have <code>rpcuser</code> and <code>rpcpassword</code> sections in kyan.conf file or if kyan.conf file is empty.</p>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          label="my-kyanite-rpcuser"
+          outlined
+          v-model="v.rpcUser"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          label="my-kyanite-rpcpassword"
+          outlined
+          v-model="v.rpcPassword"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+          <v-btn
+          color="blue-grey"
+          class="ma-2 white--text"
+          @click="next"
+          absolute
+          right
+          >
+          Next
+          <v-icon right dark>mdi-arrow-right</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+       <v-col cols="12">
+          <v-btn
+          color="blue-grey"
+          class="ma-2 white--text"
+          @click="testConnection"
+          absolute
+          right
+          >
+          Test Connection
+          <v-icon right dark>mdi-arrow-right</v-icon>
+        </v-btn>
+      </v-col>
+   </v-row>
+
+  </v-container>
+</template>
+
+<script>
+	import RpcConfig from '../plugins/dashrpc/rpc_config'
+	export default {
+		props: {
+		nextf: { type: Function },
+		},
+
+		data: () => ({
+		v: {
+			rpcUser:RpcConfig.user,
+			rpcPassword:RpcConfig.pass,
+		}
+		}),
+
+		methods: {
+		testConnection(){
+			// make a 'getnetworkinfo' rpc call to test the connection. 'getinfo' rpc call is deprecated and will be removed in the next version (soon).
+			console.log('Connection Test Run');
+		},
+		next(){
+			this.nextf(this.v)
+		}
+		}
+	}
+</script>
