5bfc9e5d407384860998259800f27432c779b431
Mustafa Ali KAHRAMAN <tafali@gmail.com>
2020-10-15 21:10:59 +0300

    BLS adimina rpc eklendi

--- src/components/BLS.vue
+++ src/components/BLS.vue
@@ -16,7 +16,16 @@
               BLS key pair
             </v-chip>  
             with <code>bls generate</code>
+            <v-btn icon color="indigo" v-if="!d.manuel" @click="blsGenerate()">
+              <v-icon>mdi-arrow-right-drop-circle</v-icon>
+            </v-btn>
         </p>
+      </v-col>
+    </v-row>
+
+    <v-row v-if="message">
+      <v-col cols="12">
+          {{message}}
       </v-col>
     </v-row>
 
@@ -63,18 +72,48 @@
   export default {
     props: {
       nextf: { type: Function },
+      d: Object
     },
 
     data: () => ({
       v: {
         blsSecret:'',
         blsPublic:''
-      }
+      },
+      message: ''
     }),
 
     methods: {
       next(){
         this.nextf(this.v)
+      },
+      blsGenerate() {
+        window.ipcRenderer.invoke('rpc', 
+          {
+            'jsonrpc': '2.0', 
+            'id': 'kmg' + parseInt(Math.random() * 100000), 
+            'method': 'bls',
+            'params': ['generate'] 
+          }
+          )
+          .then((result) => {
+            if(result.success){
+              if(!result.result.error){
+                let blso =  result.result.result
+                      /*{
+                        "secret": "52f35cd3d977a505485f2474e7e71ef3f60f859603d72ad6b0fa7f7bd163e144",
+                        "public": "885d01d746c3e4d2093b0975de2d8c1f3e5a2c3e8fdaaed929f86fc9fbb278a095248163c101a2456650b415776b7990"
+                      }*/
+                      
+                this.v.blsSecret = blso.secret
+                this.v.blsPublic = blso.public
+              } else{
+                this.message = result.result.error.code + " : " + result.result.error.message
+              }
+            } else {
+              this.message = result.result
+            }
+          })
       }
     }
   }
