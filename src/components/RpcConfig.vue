c8b6f4e94e33b7e37086a2004d9b741fec55a7ec
Bedri Özgür Güler <bedriguler@gmail.com>
2020-10-15 15:13:02 +0300

    Adds more explanations and an example to config setup

--- src/components/RpcConfig.vue
+++ src/components/RpcConfig.vue
@@ -1,22 +1,24 @@
 <template>
   <v-container>
 
-    <v-row class="text-center">
+    <v-row>
       <v-col class="mb-4">
         <v-checkbox
           v-model="v.manuel"
           label="I want to do by manuel"
         ></v-checkbox>
 
-        <p v-if="!v.manuel">Please copy and paste your <code>rpcuser</code> and <code>rpcpassword</code> 
-          for Kyanite from kyan.conf file located in Kyanite Wallet data directory. <br>
-          And add <code>server=1</code> to your kyan.conf file and restart wallet
-          <br>
-          <v-alert v-if="testResult"
-            type="error"
-          >
-          {{testResult}}
-          </v-alert>
+        <p v-if="!v.manuel"> Please copy and paste your <code>rpcuser</code> and <code>rpcpassword</code> 
+			for Kyanite from <code>kyan.conf</code> file located in Kyanite Wallet data directory. If <code>kyan.conf</code> file is empty or there is no lines for <code>rpcuser</code> and <code>rpcpassword</code> please
+			add lines for both and set a username and a strong password to them. Please also add a <code>server=1</code> line to your <code>kyan.conf</code> file and restart the wallet for it to be able to accept rpc commands. <br> <br>
+			<span style="float:left;"> Example: </span> <br>
+			<span style="float:left;"> <code> rpcuser=my-kyanite-rpc-user </code> </span> <br>
+			<span style="float:left;"> <code> rpcpassword=f8b0faff31638e7179f3e8ad4f50a753b1392b07f36fdf4756610855daf9c4ba </code> </span> <br>
+			<v-alert v-if="testResult"
+				type="error"
+			>
+			{{testResult}}
+			</v-alert>
         </p>
 
         <p v-if="v.manuel">You can copy commands and run on your wallet debug console.</p>
