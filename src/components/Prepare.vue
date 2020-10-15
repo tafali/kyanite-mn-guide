736c566cd905d364619a6396060ac419cae5d8ff
Mustafa Ali KAHRAMAN <tafali@gmail.com>
2020-10-15 22:02:50 +0300

    Prepare adimina rpc eklendi

--- src/components/Prepare.vue
+++ src/components/Prepare.vue
@@ -24,11 +24,20 @@
                {{d.addrPayout}}  
                {{d.addrFee}}  <!-- Alert user about this address. It should have coins to pay the transaction fees in it. Also alert that if this address is given blank, payoutAddress will be used for the transaction fees. -->
             </code>
+            <v-btn icon color="indigo" v-if="!d.manuel" @click="registerPrepare()">
+              <v-icon>mdi-arrow-right-drop-circle</v-icon>
+            </v-btn>
         </p>
       </v-col>
     </v-row>
 
-    <v-row>
+    <v-row v-if="message">
+      <v-col cols="12">
+          {{message}}
+      </v-col>
+    </v-row>
+
+    <v-row v-if="false">
       <v-col cols="12">
         <p>Answer should be like this:
         <br/>
@@ -106,12 +115,44 @@
         prepTx:'',
         prepCollateralAddress:'',
         prepSignMessage:''
-      }
+      },
+      message: ''
     }),
 
     methods: {
       next(){
         this.nextf(this.v)
+      },
+      registerPrepare() {
+        window.ipcRenderer.invoke('rpc', 
+          {
+            'jsonrpc': '2.0', 
+            'id': 'kmg' + parseInt(Math.random() * 100000), 
+            'method': 'protx',
+            'params': ['register_prepare', this.d.collateralHash, this.d.collateralIndex, 'ipport',
+                       this.d.addrOwner, this.d.blsPublic, this.d.addrVoting, 0, this.d.addrPayout, this.d.addrFee] 
+          }
+          )
+          .then((result) => {
+            if(result.success){
+              if(!result.result.error){
+                let po =  result.result.result
+                      /*{
+                        "tx": "030001000....",
+                        "collateralAddress": "KooQo8...",
+                        "signMessage": "KXSS7h2s...."
+                      }*/
+                this.v.prepTx = po.tx
+                this.v.prepCollateralAddress = po.collateralAddress
+                this.v.prepSignMessage = po.signMessage
+              } else{
+                this.message = result.result.error.code + " : " + result.result.error.message
+              }
+            } else {
+              let err = JSON.parse(result.result)
+              this.message = err.error.code + " : " + err.error.message
+            }
+          })
       }
     }
   }
